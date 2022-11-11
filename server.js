7e3286b3e01be581e58fe9da7377a9e5d1bb8fac
santomi3112 <adiprasetio007@gmail.com>
2022-11-11 09:02:48 +0700

    body request

--- server.js
+++ server.js
@@ -3,7 +3,26 @@ import http from 'http'
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html')
     response.statusCode = 200
-    response.end('<h1>HTTP SERVER !</h1>')
+
+    const { method } = request
+
+    if (method === 'GET') response.end('<h1>Get!</h1>')
+    if (method === 'POST') {
+        // curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"tomi\"}"
+        let body = []
+
+        request.on('data', (chunk) => {
+            body.push(chunk)
+        })
+
+        request.on('end', () => {
+            body = Buffer.concat(body).toString()
+            const { name } = JSON.parse(body)
+            response.end(`POST BY ${name}`)
+        })
+    }
+
+
 }
 
 const server = http.createServer(requestListener)
@@ -12,4 +31,4 @@ const host = 'localhost'
 
 server.listen(port, host, () => {
     console.log(`server berjalan pada http://${host}:${port}`);
-})
+})  
